6e455fc2791b9582ea0852cad18ddb23b42365ca
TommyAndersson1991 <tommy.andersson@iths.se>
2019-04-11 09:24:37 +0200

    Initial Commit

---
+++ Mindmap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mindmap
+//
+//  Created by Tommy Andersson on 2019-04-11.
+//  Copyright © 2019 Tommy Andersson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
